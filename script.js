9871fe18898a89926c54cfb13f7047c75dcb31d9
Druiz-88 <ngrayven@gmail.com>
2024-11-08 04:15:41 +0000

    created script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let animal= "Polar Bear"
+console.log("Awesome I'm now writing Javascript")
+console.log("I'm writing a 2nd line of JS...")
+console.log(animal)
+console.log("I'm still here..")
+let purchaseprice = 15.98
+console.log(purchaseprice)
+let isTuesday = false
+console.log(isTuesday)
+let PersonName = "David"
+console.log(PersonName)
